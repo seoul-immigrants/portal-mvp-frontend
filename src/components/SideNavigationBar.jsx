84225611ee071cf13aad77135ec5d317af373066
guest6 <guest6@eco.co.kr>
2023-10-18 08:56:51 +0900

    작업중...

--- src/components/SideNavigationBar.jsx
+++ src/components/SideNavigationBar.jsx
@@ -1,7 +1,7 @@
 const SideNavigationBar = ({ children }) => {
     return (
         <aside className="min-w-[310px] bg-white py-[40px] px-[50px]">
-            
+            { children }
         </aside>
     )
 }
--- src/layouts/ContentLayout.jsx
+++ src/layouts/ContentLayout.jsx
@@ -1,5 +1,33 @@
 import SideNavigationBar from "../components/SideNavigationBar";
 
+const menus = [
+    {
+        name: "MENU",
+        type: "search",
+        element: ""
+    },
+    {
+        type: "bar"
+    },
+    {
+        name: "질문과 답변",
+        type: "menu"
+    },
+    {
+        name: "자유게시판",
+        type: "menu"
+    },
+    {
+        name: "자료실",
+        type: "menu"
+    },
+    {
+        name: "PERSONAL NAVIGATOR",
+        type: "bar",
+        element: ""
+    }
+]
+
 const ContentLayuot = ({ children }) => {
     return (
         <div className="flex w-full">
